942140d324bd3ceef7713efa8de954dc4de985e3
Juliara Vasconcelos <40279322+JuliaraVasconcelos@users.noreply.github.com>
2022-10-04 11:36:42 -0300

    music portal

---
+++ assets/js/modal.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', function(e){
+    // console.log(e.target)
+    if(e.target.classList.contains('gallery-item')){
+        const src = e.target.getAttribute('src');
+        const alt = e.target.getAttribute('alt');
+
+        // console.log(src, alt);
+        document.querySelector('.modal-title').textContent = alt;
+        document.querySelector('.modal-img').src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
